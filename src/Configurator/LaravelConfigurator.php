fe2d931a7ecf6ae9e0d950334f4d8f0a22f3c023
Jan Sorgalla <jsorgalla@gmail.com>
2014-10-11 19:43:31 +0200

    Fix laravel configurator

--- src/Configurator/LaravelConfigurator.php
+++ src/Configurator/LaravelConfigurator.php
@@ -3,7 +3,9 @@
 namespace Jsor\Stack\Hal\Configurator;
 
 use Illuminate\Routing\Router;
+use Jsor\Stack\Hal\Response\HalResponse;
 use Jsor\Stack\Hal\ResponseConverter;
+use Nocarrier\Hal;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class LaravelConfigurator implements ConfiguratorInterface
@@ -12,11 +14,13 @@ class LaravelConfigurator implements ConfiguratorInterface
     {
         $app->extend('router', function (Router $router) use ($prettyPrint) {
             $router->filter('JsorStackHalResponseConverter', function ($route, $request) use ($prettyPrint) {
-                return ResponseConverter::convert(
-                    $route->run($request),
-                    $request,
-                    $prettyPrint
-                );
+                $hal = $route->run($request);
+
+                if (!$hal instanceof Hal) {
+                    return $hal;
+                }
+
+                return HalResponse::create($hal, 200, [], $prettyPrint);
             });
 
             $router->when('*', 'JsorStackHalResponseConverter');
